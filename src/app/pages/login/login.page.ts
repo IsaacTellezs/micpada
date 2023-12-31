7c496b07cb528510a7bd99cec0a074b67730d533
Isaac Tellez <andreescalante34@gmail.com>
2023-12-30 20:37:56 -0600

    Funcion reseteo de contraseña

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AutheticationService } from 'src/app/authetication.service';
 
@@ -11,7 +12,7 @@ import { AutheticationService } from 'src/app/authetication.service';
 export class LoginPage implements OnInit {
   loginForm : FormGroup
 
-  constructor(public formBuilder:FormBuilder, public loadingCtrl: LoadingController, public authService:AutheticationService) { }
+  constructor(public route: Router, public formBuilder:FormBuilder, public loadingCtrl: LoadingController, public authService:AutheticationService) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
@@ -32,11 +33,24 @@ export class LoginPage implements OnInit {
     return this.loginForm?.controls;
   }
   
-  async signUp(){
+  async login(){
     const loading = await this.loadingCtrl.create();
     await loading.present();
     if(this.loginForm?.valid){
-     // const user = await this.authService.registerUser(email,password)
+
+      const user = await this.authService.loginUser(this.loginForm.value.email,this.loginForm.value.password).catch((error)=>{
+        console.log(error);
+        loading.dismiss
+
+        
+      })
+
+      if(user){
+        loading.dismiss()
+        this.route.navigate(['/home'])
+      }else{
+        console.log('Escribe los valores correctos')
+      }
     }
   }
 
